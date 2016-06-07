53544185a060e0fd70123120a80af11bc71356d8
jerrycjw <YOUR EMAIL ADDRESS>
2016-06-07 10:16:38 -0700

    update

--- data_loader.py
+++ data_loader.py
@@ -5,10 +5,9 @@ import random
 
 def load_data(path = ['data/face_data.pkl.gz','data/jaffe.pkl.gz','data/KDEF-FACE.pkl.gz']):
     fs = map(lambda x: gzip.open(x, 'rb'),path)
-    training_data = []
-    validation_data = []
-    test_data = []
-    for f in fs:
+    training_data, validation_data, test_data = cPickle.load(fs[0])
+    for f in fs[1:]:
+
         training, validation, test = cPickle.load(f)
         for x in training: training_data[0].append(x[0]); training_data[1].append(x[1]);
         for x in validation: validation_data[0].append(x[0]); validation_data[1].append(x[1]);
