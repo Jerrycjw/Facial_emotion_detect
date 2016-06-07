1f1efe5f56b1683dda43ebb597a1b9c07d2f30f1
jerrycjw <YOUR EMAIL ADDRESS>
2016-06-07 09:02:32 -0700

    add new image processor

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,19 @@
 import cPickle
 import gzip
 import numpy as np
+import random
 
-def load_data(path = 'data/data.pkl.gz'):
-    f = gzip.open(path, 'rb')
-    training_data = cPickle.load(f)
-    f.close()
-    return (training_data)
+def load_data(path = ['data/face_data.pkl.gz','data/jaffe.pkl.gz','KDEF-FACE.pkl.gz']):
+    fs = map(lambda path: gzip.open(path, 'rb'))
+    training_data = []
+    validation_data = []
+    test_data = []
+    for f in fs:
+        training, validation, test = cPickle.load(f)
+        training_data.append(training)
+        validation_data.append(validation)
+        test_data.append(test)
+        f.close()
+    random.shuffle(training_data)
+    return (training_data, validation_data, test_data)
+
--- image_processor3.py
+++ image_processor3.py
@@ -4,14 +4,14 @@ import cPickle
 import gzip
 from scipy import misc
 import random
-# This script is used for deal with jaffe dataset
-categories ={'NE':0, 'AN':1, 'DI':3, 'FE':4, 'HA':5, 'SA':6, 'SU':7}
-image_path='data/jaffe-2/*.tiff'
+# This script is used for deal with KDEF dataset
+categories ={'NE':0, 'AN':1, 'DI':3, 'AF':4, 'HA':5, 'SA':6, 'SU':7}
+image_path='data/KDEF-2/*/*.JPG'
 pairs = []
 index = 0
 image_list = glob.glob(image_path)
 imgs = map(lambda x: misc.imread(x,'L'), image_list)
-label = map(lambda x: categories[x[16:18]], image_list)
+label = map(lambda x: categories[x[-7:-5]], image_list)
 imgs_face_small = map(lambda x: misc.imresize(x,(64,64)).reshape(64*64),imgs)
 for i in xrange(len(image_list)):
     pairs.append((imgs_face_small[i],label[i]))
@@ -19,5 +19,5 @@ training_data = [list(d) for d in zip(*pairs[:int(len(pairs)*0.8)])]
 validation_data = [list(d) for d in zip(*pairs[int(len(pairs)*0.8):int(len(pairs)*0.9)])]
 test_data = [list(d) for d in zip(*pairs[int(len(pairs)*0.9):])]
 
-f = gzip.open('data/jaffe.pkl.gz', 'w')
+f = gzip.open('data/KDEF-FACE.pkl.gz', 'w')
 cPickle.dump((training_data, validation_data, test_data), f)
