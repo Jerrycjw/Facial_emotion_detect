71fd6a37b759010937d4163318b36ac9c3b3435d
jerrycjw <YOUR EMAIL ADDRESS>
2016-06-07 21:47:12 -0700

    fix random shuffle

--- data_loader.py
+++ data_loader.py
@@ -3,12 +3,10 @@ import gzip
 import numpy as np
 import random
 from sklearn import cross_validation
-def load_data(path = ['data/face_data_2D.pkl.gz','data/jaffe_2D.pkl.gz','data/KDEF-FACE_2D.pkl.gz']):
-    dataset = map(lambda x: cPickle.load(gzip.open(x, 'rb')),path)
-    X = dataset[0][0]+dataset[1][0]+dataset[2][0]
-    Y = dataset[0][1]+dataset[1][1]+dataset[2][1]
-    random.shuffle(X)
-    random.shuffle(Y)
+def load_data(path ='dataset_2D_64.pkl.gz'):
+    data = cPickle.load(gzip.open(path))
+    X = data[0]
+    Y = data[1]
     X_train, X_dev, Y_train, Y_dev = cross_validation.train_test_split(X,Y,test_size=0.2)
     X_test, X_val, Y_test, Y_val = cross_validation.train_test_split(X_dev,Y_dev,test_size=0.5)
     return ([X_train,Y_train], [X_val,Y_val], [X_test, Y_test])
