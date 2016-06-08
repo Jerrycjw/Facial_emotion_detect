b42e4ceace7fa1f6f639967f3192a137b76a6135
jerrycjw <YOUR EMAIL ADDRESS>
2016-06-07 18:29:41 -0700

    add validation and testing

--- keras_vgg.py
+++ keras_vgg.py
@@ -33,9 +33,14 @@ if __name__ == '__main__':
     import data_loader
     train, valid, test = data_loader.load_data(['data/face_data_2D.pkl.gz','data/jaffe_2D.pkl.gz','data/KDEF-FACE_2D.pkl.gz'])
     model = VGG_16()
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    sgd = SGD(lr=0.005, decay=1e-6, momentum=0.005, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
-    data = np.array(train[0]).reshape(len(train[0]),1,64,64)
+    X_train = np.array(train[0]).reshape(len(train[0]),1,64,64)
+    X_valid =  np.array(valid[0]).reshape(len(valid[0]),1,64,64)
+    X_test = np.array(test[0]).reshape(len(test[0]),1,64,64)
     from keras.utils.np_utils import to_categorical
-    labels = to_categorical(np.array(train[1]))
-    out = model.fit(data,labels,nb_epoch=100,batch_size=30)
+    Y_train = to_categorical(np.array(train[1]))
+    Y_valid = to_categorical(np.array(valid[1]))
+    Y_test = to_categorical(np.array(test[1]))
+    train_out = model.fit(X_train,Y_train,nb_epoch=100,batch_size=30,validation_data=(X_valid,Y_valid))
+    test_out = model.evaluate(X_test, Y_test, batch_size=32, verbose=1, sample_weight=None)
