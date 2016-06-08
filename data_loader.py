261848d1967c21872d50c874f320152e6c139c1a
jerrycjw <YOUR EMAIL ADDRESS>
2016-06-07 20:37:21 -0700

    now data is shuffled

--- data_loader.py
+++ data_loader.py
@@ -2,19 +2,16 @@ import cPickle
 import gzip
 import numpy as np
 import random
-
-def load_data(path = ['data/face_data.pkl.gz','data/jaffe.pkl.gz','data/KDEF-FACE.pkl.gz']):
-    fs = map(lambda x: gzip.open(x, 'rb'),path)
-    training_data, validation_data, test_data = cPickle.load(fs[0])
-
-    for f in fs[1:]:
-        print(len(training_data[0]))
-        training, validation, test = cPickle.load(f)
-        for x in xrange(len(training[0])): training_data[0].append(training[0][x]); training_data[1].append(training[1][x]);
-        for x in xrange(len(validation[0])): validation_data[0].append(validation[0][x]); validation_data[1].append(validation[1][x]);
-        for x in xrange(len(validation[0])): test_data[0].append(test[0][x]); test_data[1].append(test[1][x]);
-        f.close()
-    return (training_data, validation_data, test_data)
+from sklearn import cross_validation
+def load_data(path = ['data/face_data_2D.pkl.gz','data/jaffe_2D.pkl.gz','data/KDEF-FACE_2D.pkl.gz']):
+    dataset = map(lambda x: cPickle.load(gzip.open(x, 'rb')),path)
+    X = dataset[0][0]+dataset[1][0]+dataset[2][0]
+    Y = dataset[0][1]+dataset[1][1]+dataset[2][1]
+    random.shuffle(X)
+    random.shuffle(Y)
+    X_train, X_dev, Y_train, Y_dev = cross_validation.train_test_split(X,Y,test_size=0.2)
+    X_test, X_val, Y_test, Y_val = cross_validation.train_test_split(X_dev,Y_dev,test_size=0.5)
+    return ([X_train,Y_train], [X_val,Y_val], [X_test, Y_test])
 
 if __name__ == '__main__':
     load_data()
